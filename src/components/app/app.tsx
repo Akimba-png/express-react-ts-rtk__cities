e966f7a2e96a1dd4f049bb3be2c2df60c023de0d
Oleg <akimba@gmail.com>
2022-07-01 18:56:18 +0300

    Добавляет компонент страницы 404

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,12 @@
 // import FavoritesPage from '../../pages/favorites-page/favorites-page';
 // import MainPage from '../../pages/main-page/main-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
-import PropertyPage from '../../pages/property-page/property-page';
+// import PropertyPage from '../../pages/property-page/property-page';
 
 function App(): JSX.Element {
-  return <PropertyPage />;
+  // return <MainPage />;
+  return <NotFoundPage />;
 }
 
 export default App;
