bd4e8657c4ec76c5ae9302e3c0d99335daf6de52
Oleg <akimba@gmail.com>
2022-08-28 17:52:05 +0300

    feat: Добавляет линк Favorite-Main с фильтрацией по указанному городу
    Осуществляет переход со странице Favorite на страницу Main
    при клике на название города. При переходе, на странице Main,
    сразу применяется фильтрация по данному городу.

--- src/components/cards/favorite-list/favorite-list.tsx
+++ src/components/cards/favorite-list/favorite-list.tsx
@@ -1,26 +1,39 @@
-import { useSelector } from 'react-redux';
+import { MouseEvent } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppDispatch } from '../../../types/thunk';
 import { getFavoriteOffers } from '../../../store/app-data/selector';
+import { setActiveFilter, redirectToPage } from '../../../store/action';
 import FavoritesCard from '../favorites-card/favorites-card';
 import { getOffersByCity } from './../../../util';
-import { CITIES } from './../../../const';
+import { AppRoute, CITIES } from './../../../const';
 
 
 function FavoriteList(): JSX.Element {
 
+  const dispatch = useDispatch() as AppDispatch;
   const offers = useSelector(getFavoriteOffers);
   const offersByCity = getOffersByCity(offers);
   const favoriteCities = Object.keys(offersByCity);
   const sortedFavoriteCities = CITIES.filter((city) => favoriteCities.includes(city));
 
+  const handleCityClick = (city: string) => (evt: MouseEvent) => {
+    evt.preventDefault();
+    dispatch(setActiveFilter(city));
+    dispatch(redirectToPage(AppRoute.Main));
+  };
+
   return (
     <ul className="favorites__list">
-      {sortedFavoriteCities.map((city, i, a) => {
+      {sortedFavoriteCities.map((city, i) => {
         const keyValue = city + i;
         return (
           <li className="favorites__locations-items" key={keyValue}>
             <div className="favorites__locations locations locations--current">
               <div className="locations__item">
-                <a className="locations__item-link" href="#">
+                <a
+                  onClick={handleCityClick(city)}
+                  className="locations__item-link" href="/#"
+                >
                   <span>{city}</span>
                 </a>
               </div>
