884a9d3a10e336b84e0333ff18b26e9b8992161b
Oleg Akimov <akimba@gmail.com>
2024-02-26 19:18:08 +0300

    server: Добавляет middleware для отдачи изображений

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { hotelRouter } from './src/routers/hotel-router';
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('src'));
 app.use('/', (req, res) => { res.send('hello')});
 
 app.listen(PORT, () => console.log(`App is listeninig on port ${PORT}`));
